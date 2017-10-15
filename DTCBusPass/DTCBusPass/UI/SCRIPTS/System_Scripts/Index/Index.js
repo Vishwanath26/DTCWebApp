ba4c31606626837192a052286ece68bb1aaa8fdb
Vishwanath26 <vishwanath.sharma724@gmail.com>
2017-10-15 14:00:54 +0530

    Ui complete design

--- DTCBusPass/DTCBusPass/UI/SCRIPTS/System_Scripts/Index/Index.js
+++ DTCBusPass/DTCBusPass/UI/SCRIPTS/System_Scripts/Index/Index.js
@@ -1,4 +1,37 @@
 ﻿$(document).ready(function () {
+    $("#errorDiv").css("display", "none");
+    $('#input-userName').unbind().keyup(function (e) {
+        var EmailTextBox = document.getElementById("input-userName");
+        ValidateElementUsingRegex(EmailTextBox,"email");
+    });
 
+    $('#LoginButtonDiv').click(function () {
+        alert('Site In Progress');
+    });
+    $('#SubmitButtonDiv').click(function () {
+        alert('Congratulations!! You are registered for a DTC bus pass');
+    });
+    $('#input-FirstName').unbind().keyup(function (e) {
+        var fNameElem = document.getElementById("input-FirstName");
+        ValidateElementUsingRegex(fNameElem);
+    });
+    $('#input-SurName').unbind().keyup(function (e) {
+        var SNameElem = document.getElementById("input-SurName");
+        ValidateElementUsingRegex(SNameElem);
+    });
 
+    ValidateElementUsingRegex = function (elem,type) {
+        var elemVal = elem.value;
+        var regex = (type == "email") ? /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ : /^[a-zA-Z ]*$/;        
+        if (!regex.test(elemVal)) {
+            elem.style.border = "2px solid red";
+        }
+        else {
+            elem.style.border = "none";
+        }
+    };
+    $('#input-UserEmail').unbind().keyup(function (e) {
+        var EmailElem = document.getElementById("input-UserEmail");
+        ValidateElementUsingRegex(EmailElem,"email");
+    });    
 });
